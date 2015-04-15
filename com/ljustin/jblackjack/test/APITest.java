e05398ad5a546898674f8beda898d76649c80f78
ljustin <ljustin8925@yahoo.com>
2015-04-14 20:40:35 -0400

    Added a test package and ant build file to generate a jar.

---
+++ com/ljustin/jblackjack/test/APITest.java
@@ -0,0 +1,22 @@
+package com.ljustin.jblackjack.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class APITest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
